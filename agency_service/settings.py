3fa7494ea74c96369f06cef3b9a6a13493395f04
Valerii Kashpur <kashpur.v.f@gmail.com>
2025-02-03 15:53:37 +0200

    chore: added templates & debug

--- agency_service/settings.py
+++ agency_service/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'press',
 ]
 
@@ -60,7 +61,7 @@ ROOT_URLCONF = 'agency_service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
