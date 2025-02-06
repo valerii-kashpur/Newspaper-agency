fc9a0237b8ac445fd55a08cc91fd5e3305b5ecc4
Valerii Kashpur <kashpur.v.f@gmail.com>
2025-02-06 14:17:03 +0200

    build: static config

--- agency_service/settings/base.py
+++ agency_service/settings/base.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -115,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "staticfiles"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
