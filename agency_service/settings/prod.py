4164ca3964960552fe14e5a51362c523aebda68d
Valerii Kashpur <kashpur.v.f@gmail.com>
2025-02-06 14:24:42 +0200

    build: settings update

--- agency_service/settings/prod.py
+++ agency_service/settings/prod.py
@@ -4,7 +4,11 @@ from agency_service.settings.base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.01", "localhost"]
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
