3099f2f5285ac025012ea4ec759aab9d23d981b6
Valerii Kashpur <kashpur.v.f@gmail.com>
2025-02-06 18:38:17 +0200

    Revert "fix: ALLOWED_HOSTS"
    This reverts commit c369e92b5e2269f1ee6e3e2487572636ccbabb0a.

--- agency_service/settings/prod.py
+++ agency_service/settings/prod.py
@@ -6,8 +6,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["agency-old-paper.onrender.com"]
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME',
-                                          "agency-old-paper.onrender.com")
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
